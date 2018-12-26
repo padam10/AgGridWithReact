0ce946abd61ed132ab216ffd632f62d5854f6745
Padam Bhandari <Padam.Bhandari@datascan.com>
2018-12-26 17:40:35 -0600

    Added AgGrid sorting, filtering and  pagination

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            columnDefs: [
+                {headerName: 'Make', field: 'make'},
+                {headerName: 'Model', field: 'model'},
+                {headerName: 'Price', field: 'price', editable:true}
+
+            ],
+            rowData: [
+                {make: 'Toyota', model: 'Celica', price: 35000},
+                {make: 'Ford', model: 'Mondeo', price: 32000},
+                {make: 'Porsche', model: 'Boxter', price: 72000}
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div
+                className="ag-theme-balham"
+                style={{ height: '200px', width: '600px' }}
+            >
+                <AgGridReact
+                    enableFilter = {true}
+                    enableSorting={true}
+                    pagination = {true}
+                    columnDefs={this.state.columnDefs}
+                    rowData={this.state.rowData}>
+                </AgGridReact>
+            </div>
+        );
+    }
 }
 
 export default App;
